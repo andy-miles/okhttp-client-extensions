cef787309146e6fce6ba0a1946540dc12f489bfb
Andrew Miles <35545964+andy-miles@users.noreply.github.com>
2025-11-08 12:52:58 -0800

    Added ProgressTrackingInputStream

---
+++ src/main/java/com/amilesend/client/connection/file/ProgressTrackingInputStream.java
@@ -0,0 +1,139 @@
+package com.amilesend.client.connection.file;
+
+import com.amilesend.client.util.VisibleForTesting;
+import lombok.NonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+/** Tracking input stream to monitor the amount of bytes read. */
+public class ProgressTrackingInputStream extends InputStream {
+    private static final int DEFAULT_BUFFER_SIZE = 16384;
+
+    /** The counter keeping track of the number of bytes transferred between IO sinks. */
+    private final AtomicLong bytesTransferred = new AtomicLong(0L);
+
+    /** The delegate input stream that is being tracked. */
+    @NonNull
+    private final InputStream delegate;
+    /**
+     * The callback to notify.
+     *
+     * @see TransferProgressCallback
+     */
+    private final Optional<TransferProgressCallback> callback;
+    /** The total number of bytes to transfer. */
+    private final long totalBytes;
+
+    /**
+     * Creates a new {@code ProgressTrackingInputStream} object.
+     *
+     * @param delegate the delegate input stream
+     * @param callback the callback to notify
+     * @param totalBytes the total bytes to read
+     */
+    public ProgressTrackingInputStream(
+            @NonNull final InputStream delegate,
+            final TransferProgressCallback callback,
+            final long totalBytes) {
+        this.delegate = delegate;
+        this.callback = Optional.ofNullable(callback);
+        this.totalBytes = totalBytes;
+    }
+
+    @Override
+    public int read() throws IOException {
+        try {
+            final int byteValue = delegate.read();
+            addProgressAndNotify(1L);
+            return byteValue;
+        } catch (final IOException ex) {
+            callback.ifPresent(c -> c.onFailure(ex));
+            throw ex;
+        }
+    }
+
+    @Override
+    public int read(@NonNull final byte[] b) throws IOException {
+        try {
+            final int bytesRead = delegate.read(b);
+            addProgressAndNotify(bytesRead);
+            return bytesRead;
+        } catch (final IOException ex) {
+            callback.ifPresent(c -> c.onFailure(ex));
+            throw ex;
+        }
+    }
+
+    @Override
+    public int read(final byte[] b, final int off, final int len) throws IOException {
+        try {
+            final int bytesRead = delegate.read(b, off, len);
+            addProgressAndNotify(bytesRead);
+            return bytesRead;
+        } catch (final IOException ex) {
+            callback.ifPresent(c -> c.onFailure(ex));
+            throw ex;
+        }
+    }
+
+    @Override
+    public byte[] readAllBytes() throws IOException {
+        try {
+            final byte[] data = delegate.readAllBytes();
+            addProgressAndNotify(data.length);
+            return data;
+        } catch (final IOException ex) {
+            callback.ifPresent(c -> c.onFailure(ex));
+            throw ex;
+        }
+    }
+
+    @Override
+    public byte[] readNBytes(final int len) throws IOException {
+        try {
+            final byte[] data = delegate.readNBytes(len);
+            addProgressAndNotify(data.length);
+            return data;
+        } catch (final IOException ex) {
+            callback.ifPresent(c -> c.onFailure(ex));
+            throw ex;
+        }
+    }
+
+    @Override
+    public int readNBytes(final byte[] b, final int off, final int len) throws IOException {
+        try {
+            final int bytesRead = delegate.readNBytes(b, off, len);
+            addProgressAndNotify(bytesRead);
+            return bytesRead;
+        } catch (final IOException ex) {
+            callback.ifPresent(c -> c.onFailure(ex));
+            throw ex;
+        }
+    }
+
+    @Override
+    public long skip(final long n) throws IOException {
+        try {
+            final long numSkippedBytes = delegate.skip(n);
+            addProgressAndNotify(numSkippedBytes);
+            return numSkippedBytes;
+        } catch (final IOException ex) {
+            callback.ifPresent(c -> c.onFailure(ex));
+            throw ex;
+        }
+    }
+
+    @VisibleForTesting
+    void addProgressAndNotify(final long bytesRead) {
+        final long totalBytesRead = bytesTransferred.addAndGet(bytesRead);
+        callback.ifPresent(c -> c.onUpdate(totalBytesRead, totalBytes));
+
+        if (totalBytesRead >= totalBytes) {
+            callback.ifPresent(c -> c.onComplete(totalBytesRead));
+        }
+    }
+}
---
+++ src/test/java/com/amilesend/client/connection/file/ProgressTrackingInputStreamTest.java
@@ -0,0 +1,322 @@
+package com.amilesend.client.connection.file;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProgressTrackingInputStreamTest {
+    private static final long TOTAL_BYTES = 100L;
+
+    @Mock
+    private InputStream mockDelegate;
+    @Mock
+    private TransferProgressCallback mockCallback;
+
+    private ProgressTrackingInputStream inputStreamUnderTest;
+
+    @BeforeEach
+    public void setUp() {
+        inputStreamUnderTest = spy(new ProgressTrackingInputStream(mockDelegate, mockCallback, TOTAL_BYTES));
+    }
+
+    ///////////
+    // read()
+    ///////////
+
+    @Test
+    @SneakyThrows
+    public void read_withNoException_shouldReturnByte() {
+        final int expected = 8;
+        when(mockDelegate.read()).thenReturn(expected);
+
+        final int actual = inputStreamUnderTest.read();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withNoException_shouldNotifyCallback() {
+        read_withNoException_shouldReturnByte();
+        verify(inputStreamUnderTest).addProgressAndNotify(eq(1L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withIOException_shouldThrowException() {
+        when(mockDelegate.read()).thenThrow(new IOException("Exception"));
+
+        assertThrows(IOException.class, () -> inputStreamUnderTest.read());
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withIOException_shouldNotifyCallback() {
+        read_withIOException_shouldThrowException();
+        verify(mockCallback).onFailure(isA(IOException.class));
+    }
+
+    /////////////////
+    // read(byte[])
+    /////////////////
+
+    @Test
+    @SneakyThrows
+    public void read_withBytes_shouldReturnNumBytesRead() {
+        final int expected = 10;
+        when(mockDelegate.read(any(byte[].class))).thenReturn(expected);
+
+        final int actual = inputStreamUnderTest.read(new byte[expected]);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withBytes_shouldNotifyCallback() {
+        read_withBytes_shouldReturnNumBytesRead();
+        verify(inputStreamUnderTest).addProgressAndNotify(eq(10L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withBytesAndIOException_shouldThrowException() {
+        when(mockDelegate.read(any(byte[].class))).thenThrow(new IOException("Exception"));
+
+        assertThrows(IOException.class, () -> inputStreamUnderTest.read(new byte[10]));
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withBytesAndIOException_shouldNotifyCallback() {
+        read_withBytesAndIOException_shouldThrowException();
+        verify(mockCallback).onFailure(isA(IOException.class));
+    }
+
+    ///////////////////////////
+    // read(byte[], int, int)
+    ///////////////////////////
+
+    @Test
+    @SneakyThrows
+    public void read_withBytesAndOffset_shouldReturnNumBytesRead() {
+        final int expected = 10;
+        when(mockDelegate.read(any(byte[].class), anyInt(), anyInt())).thenReturn(expected);
+
+        final int actual = inputStreamUnderTest.read(new byte[expected], 0, 10);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withBytesAndOffset_shouldNotifyCallback() {
+        read_withBytesAndOffset_shouldReturnNumBytesRead();
+        verify(inputStreamUnderTest).addProgressAndNotify(eq(10L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withBytesAndOffsetAndIOException_shouldThrowException() {
+        when(mockDelegate.read(any(byte[].class), anyInt(), anyInt())).thenThrow(new IOException("Exception"));
+
+        assertThrows(IOException.class, () -> inputStreamUnderTest.read(new byte[10], 0, 10));
+    }
+
+    @Test
+    @SneakyThrows
+    public void read_withBytesAndOffsetAndIOException_shouldNotifyCallback() {
+        read_withBytesAndOffsetAndIOException_shouldThrowException();
+        verify(mockCallback).onFailure(isA(IOException.class));
+    }
+
+    ///////////////////
+    // readAllBytes()
+    ///////////////////
+
+    @Test
+    @SneakyThrows
+    public void readAllBytes_withNoException_shouldReturnBytes() {
+        final byte[] expected = new byte[] {1, 2, 3, 4};
+        when(mockDelegate.readAllBytes()).thenReturn(expected);
+
+        final byte[] actual = inputStreamUnderTest.readAllBytes();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SneakyThrows
+    public void readAllBytes_withNoException_shouldNotifyCallback() {
+        readAllBytes_withNoException_shouldReturnBytes();
+        verify(inputStreamUnderTest).addProgressAndNotify(eq(4L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void readAllBytes_withIOException_shouldThrowException() {
+        when(mockDelegate.readAllBytes()).thenThrow(new IOException("Exception"));
+
+        assertThrows(IOException.class, () -> inputStreamUnderTest.readAllBytes());
+    }
+
+    @Test
+    @SneakyThrows
+    public void readAllBytes_withIOException_shouldNotifyCallback() {
+        readAllBytes_withIOException_shouldThrowException();
+        verify(mockCallback).onFailure(isA(IOException.class));
+    }
+
+    ////////////////////
+    // readNBytes(int)
+    ////////////////////
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withLength_shouldReturnBytes() {
+        final byte[] expected = new byte[] {1, 2, 3, 4};
+        when(mockDelegate.readNBytes(anyInt())).thenReturn(expected);
+
+        final byte[] actual = inputStreamUnderTest.readNBytes(4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withLength_shouldNotifyCallback() {
+        readNBytes_withLength_shouldReturnBytes();
+        verify(inputStreamUnderTest).addProgressAndNotify(eq(4L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withIOException_shouldThrowException() {
+        when(mockDelegate.readNBytes(anyInt())).thenThrow(new IOException("Exception"));
+
+        assertThrows(IOException.class, () -> inputStreamUnderTest.readNBytes(4));
+    }
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withIOException_shouldNotifyCallback() {
+        readNBytes_withIOException_shouldThrowException();
+        verify(mockCallback).onFailure(isA(IOException.class));
+    }
+
+    ///////////////////////////////
+    // readNBytes(byte, int, int)
+    ///////////////////////////////
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withBytesAndOffset_shouldReturnNumBytesRead() {
+        final int expected = 10;
+        when(mockDelegate.readNBytes(any(byte[].class), anyInt(), anyInt())).thenReturn(expected);
+
+        final int actual = inputStreamUnderTest.readNBytes(new byte[expected], 0, expected);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withBytesAndOffset_shouldNotifyCallback() {
+        readNBytes_withBytesAndOffset_shouldReturnNumBytesRead();
+        verify(inputStreamUnderTest).addProgressAndNotify(eq(10L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withBytesAndOffsetAndIOException_shouldThrowException() {
+        when(mockDelegate.readNBytes(any(byte[].class), anyInt(), anyInt())).thenThrow(new IOException("Exception"));
+
+        assertThrows(IOException.class, () -> inputStreamUnderTest.readNBytes(new byte[10], 0, 10));
+    }
+
+    @Test
+    @SneakyThrows
+    public void readNBytes_withBytesAndOffsetAndIOException_shouldNotifyCallback() {
+        readNBytes_withBytesAndOffsetAndIOException_shouldThrowException();
+        verify(mockCallback).onFailure(isA(IOException.class));
+    }
+
+    /////////
+    // skip
+    /////////
+
+    @Test
+    @SneakyThrows
+    public void skip_withNumBytes_shouldReturnNumBytesSkipped() {
+        final long expected = 10L;
+        when(mockDelegate.skip(anyLong())).thenReturn(expected);
+
+        final long actual = inputStreamUnderTest.skip(expected);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SneakyThrows
+    public void skip_withNumBytes_shouldNotifyCallback() {
+        skip_withNumBytes_shouldReturnNumBytesSkipped();
+        verify(inputStreamUnderTest).addProgressAndNotify(eq(10L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void skip_withNumBytesAndIOException_shouldThrowException() {
+        when(mockDelegate.skip(anyLong())).thenThrow(new IOException("Exception"));
+
+        assertThrows(IOException.class, () -> inputStreamUnderTest.skip(10L));
+    }
+
+    @Test
+    @SneakyThrows
+    public void skip_withNumBytesAndIOException_shouldNotifyCallback() {
+        skip_withNumBytesAndIOException_shouldThrowException();
+        verify(mockCallback).onFailure(isA(IOException.class));
+    }
+
+    /////////////////////////
+    // addProgressAndNotify
+    /////////////////////////
+
+    @Test
+    public void addProgressAndNotify_withLessThanTotalBytesRead_shouldNotifyCallback() {
+        inputStreamUnderTest.addProgressAndNotify(50L);
+
+        verify(mockCallback).onUpdate(eq(50L), eq(TOTAL_BYTES));
+    }
+
+    @Test
+    public void addProgressAndNotify_withEqualToTotalBytesRead_shouldNotifyCallback() {
+        inputStreamUnderTest.addProgressAndNotify(TOTAL_BYTES);
+
+        assertAll(
+                () -> verify(mockCallback).onUpdate(eq(TOTAL_BYTES), eq(TOTAL_BYTES)),
+                () -> verify(mockCallback).onComplete(eq(TOTAL_BYTES)));
+    }
+}
